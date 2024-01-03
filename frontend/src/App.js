51832d55ea2c436cb09c1a262cb50ff69b1c5f1d
HeshanRazor <heshru@gmail.com>
2024-01-03 23:43:37 +0100

    controlled serch component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,21 @@
+import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
+import Search from './components/Search';
 
-const App =() => {
+
+const App = () => {
+  const [word, setword] = useState('');
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    console.log(word);
+  };
   return (
     <div className="App">
-        <Header title = "Images Gallery"/>
+      <Header title="Images Gallery" />
+      <Search word={word} setword={setword} handleSubmit={handleSearchSubmit} />
     </div>
   );
 }
 
 export default App;
- 
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
-import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 
+const navbarStyle = {
+    backgroundColor :"lightblue"
+};
 
 const Header = (props) => {
     return(
-        <Navbar expand="lg" className="bg-body-tertiary">
-        <Container fluid>
-          <Navbar.Brand href="/">{props.title}</Navbar.Brand>
-          <Navbar.Toggle aria-controls="navbarScroll" />   
+        <Navbar style={navbarStyle} >
+        <Container>
+          <Navbar.Brand href="#home">{props.title}</Navbar.Brand>
+          <Navbar.Toggle />
         </Container>
       </Navbar>
     );
