f567b7773de1ab4cbe7f3a9011a5e0f5452343af
HeshanRazor <heshru@gmail.com>
2024-01-11 01:17:30 +0100

    frist API call

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,10 @@
-import { useState } from 'react';
+import { startTransition, useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
+
+const UNPLEASH_KEY = process.env.REACT_APP_UNPLEASH_KEY
+//const UNPLEASH_KEY ='xQNZHWbeza-B0eHAQFuDaNE_39LoRIRu-_4heDzVjoY'
 
 
 const App = () => {
@@ -9,7 +12,18 @@ const App = () => {
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word);
-  };
+    //console.log(UNPLEASH_KEY);
+    fetch(`https://api.unsplash.com/photos/random?query=${word}&client_id=${UNPLEASH_KEY}`)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    
+  }
+  //console.log(UNPLEASH_KEY);
   return (
     <div className="App">
       <Header title="Images Gallery" />
