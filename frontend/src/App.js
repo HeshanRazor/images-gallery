72c64226635cd297381307fdc753b7dc1e1f865e
HeshanRazor <heshru@gmail.com>
2024-01-15 23:03:29 +0100

    eslint and prettier set up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,17 +3,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
 import Search from './components/Search';
 
-const UNPLEASH_KEY = process.env.REACT_APP_UNPLEASH_KEY
+const UNPLEASH_KEY = process.env.REACT_APP_UNPLEASH_KEY;
 //const UNPLEASH_KEY ='xQNZHWbeza-B0eHAQFuDaNE_39LoRIRu-_4heDzVjoY'
-
-
 const App = () => {
   const [word, setword] = useState('');
   const handleSearchSubmit = (e) => {
     e.preventDefault();
     console.log(word);
     //console.log(UNPLEASH_KEY);
-    fetch(`https://api.unsplash.com/photos/random?query=${word}&client_id=${UNPLEASH_KEY}`)
+    fetch(
+      `https://api.unsplash.com/photos/random?query=${word}&client_id=${UNPLEASH_KEY}`,
+    )
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
@@ -21,8 +21,9 @@ const App = () => {
       .catch((err) => {
         console.log(err);
       });
-    
-  }
+    setword('');
+  };
+
   //console.log(UNPLEASH_KEY);
   return (
     <div className="App">
@@ -30,6 +31,6 @@ const App = () => {
       <Search word={word} setword={setword} handleSubmit={handleSearchSubmit} />
     </div>
   );
-}
+};
 
 export default App;
